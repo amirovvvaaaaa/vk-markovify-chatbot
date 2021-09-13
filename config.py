26f598fda475737ce8a6da63aed0385a52365f23
amirovvvaaaaa <90635760+amirovvvaaaaa@users.noreply.github.com>
2021-09-13 18:41:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-BOT_TOKEN = ""
+BOT_TOKEN = "ff75eeabe68a9c14e28c87e9862ecf7cad5bcfeadc1dabb51a1991e16fcf236aab05059a161681ccebae8"
 
 # Задержка перед ответом бота в секундах.
 RESPONSE_DELAY = 0
